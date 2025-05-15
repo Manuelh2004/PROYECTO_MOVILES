b24e2568cbb00016b07220937016b8d50c828763
abraham <abraham2004hf@gmail.com>
2025-05-14 22:57:40 -0500

    Finalizacion de organizacion de archivos

---
+++ app/src/main/java/com/example/proyecto_moviles/ui/Inicio/login_temporal.java
@@ -0,0 +1,38 @@
+package com.example.proyecto_moviles.ui.Inicio;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.proyecto_moviles.R;
+
+public class login_temporal extends Fragment implements View.OnClickListener{
+    private Button btnIngresoDirecto;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_login_temporal, container, false);
+
+        btnIngresoDirecto = (Button) rootView.findViewById(R.id.btnIngresoDirecto);
+        btnIngresoDirecto.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnIngresoDirecto){
+            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.action_nav_login_to_nav_presupuesto);
+        }
+    }
+}
