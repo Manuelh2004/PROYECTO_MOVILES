cdb07e3d0e8c0b0d42129199397c599ab730aceb
abraham <abraham2004hf@gmail.com>
2025-06-11 23:05:57 -0500

    Diseño del crear cuenta

--- app/src/main/java/com/example/proyecto_moviles/ui/Presupuesto/VistaPresupuesto.java
+++ app/src/main/java/com/example/proyecto_moviles/ui/Presupuesto/VistaPresupuesto.java
@@ -243,6 +243,7 @@ public class VistaPresupuesto extends Fragment implements AdapterView.OnItemClic
 
         // Crear un objeto RequestParams para almacenar los parámetros
         RequestParams params = new RequestParams();
+        params.put("id_Usuario",id_usuario);
 
         // Crear una instancia de AsyncHttpClient
         AsyncHttpClient client = new AsyncHttpClient();
@@ -296,7 +297,7 @@ public class VistaPresupuesto extends Fragment implements AdapterView.OnItemClic
                 String errorMessage = (responseBody != null) ? new String(responseBody) : error.getMessage();
                 Toast.makeText(getActivity(), "Error: " + errorMessage, Toast.LENGTH_LONG).show();
             }
-        });
+     });
     }
 
     private void EditarPaciente(String idPresupuesto) {
