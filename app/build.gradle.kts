8a1f6aa4e2da3c6ba7bcf39a05043404676da1e9
Nikol <nikolcarrascodelacruz@gmail.com>
2025-06-13 13:27:05 -0500

    resumen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("com.android.volley:volley:1.2.1")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+
 }
