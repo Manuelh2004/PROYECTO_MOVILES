799edbf26e93949125e339ec74130fd01d3c8895
abraham <abraham2004hf@gmail.com>
2025-06-15 22:58:36 -0500

    Avance del CRUD para items

--- app/src/main/java/com/example/proyecto_moviles/ui/Administrador/Item/ItemCRUD.java
+++ app/src/main/java/com/example/proyecto_moviles/ui/Administrador/Item/ItemCRUD.java
@@ -38,8 +38,8 @@ public class ItemCRUD extends Fragment implements View.OnClickListener{
     private CategoriasAdapter categoriasAdapter;
     String servidor = "http://10.0.2.2/proyecto_moviles/controladores/";
 
-    Button btnAgregarCategoria;
-    EditText editTextCategoria;
+    Button btnAgregarCategoria, btnAgregarGenero, btnAgregarTipoDocumento;
+    EditText editTextCategoria, editTextGenero, editTextTipoDocumento;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,9 +49,17 @@ public class ItemCRUD extends Fragment implements View.OnClickListener{
 
         client = new AsyncHttpClient();
 
-        // Encuentra las vistas
+        // Categorias
         editTextCategoria = rootView.findViewById(R.id.editTextCategoria);
         btnAgregarCategoria = rootView.findViewById(R.id.btnAgregarCategoria);
+
+        // Genero
+        editTextGenero = rootView.findViewById(R.id.editTextGenero);
+        btnAgregarGenero = rootView.findViewById(R.id.btnAgregarGenero);
+
+        // Tipo Documento
+        editTextTipoDocumento = rootView.findViewById(R.id.editTextTipoDocumento);
+        btnAgregarTipoDocumento = rootView.findViewById(R.id.btnAgregarTipoDocumento);
 
         // RecyclerView recyclerViewCategorias = rootView.findViewById(R.id.recyclerViewCategorias);
         CardView cardView1 = rootView.findViewById(R.id.card_view_1);  // Cambiado a CardView
@@ -63,6 +71,8 @@ public class ItemCRUD extends Fragment implements View.OnClickListener{
 
         // Manejador del botón Agregar
         btnAgregarCategoria.setOnClickListener(this);
+        btnAgregarGenero.setOnClickListener(this);
+        btnAgregarTipoDocumento.setOnClickListener(this);
 
         obtenerCategoriasDesdeServidor();
 
@@ -116,7 +126,25 @@ public class ItemCRUD extends Fragment implements View.OnClickListener{
                 // Enviar la nueva categoría al servidor
                 agregarCategoria();
             } else {
-                Toast.makeText(getContext(), "Ingresa una categoría", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Ingresa una nueva categoría", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (v == btnAgregarGenero) {
+            String nuevoGenero = editTextGenero.getText().toString().trim();
+            if (!nuevoGenero.isEmpty()) {
+                agregarGenero();
+            } else {
+                Toast.makeText(getContext(), "Ingresa un nuevo genero", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (v == btnAgregarTipoDocumento) {
+            String nuevoTipoDocumento = editTextTipoDocumento.getText().toString().trim();
+            if (!nuevoTipoDocumento.isEmpty()) {
+                agregarTipoDocumento();
+            } else {
+                Toast.makeText(getContext(), "Ingresa un nuevo tipo de documento", Toast.LENGTH_SHORT).show();
             }
         }
 
@@ -161,12 +189,10 @@ public class ItemCRUD extends Fragment implements View.OnClickListener{
     private void agregarGenero() {
         String url = servidor + "itemsController/genero/agregar_genero.php";
 
-        String nom_categoria = editTextCategoria.getText().toString();
-        Integer est_categoria = 1;
+        String nom_genero = editTextGenero.getText().toString();
 
         RequestParams params = new RequestParams();
-        params.put("nom_categoria", nom_categoria);
-        params.put("est_categoria", est_categoria);
+        params.put("nom_genero", nom_genero);
 
         client.post(url, params, new AsyncHttpResponseHandler() {
             @Override
@@ -175,14 +201,44 @@ public class ItemCRUD extends Fragment implements View.OnClickListener{
                 if (response.contains("success")) {
                     // Actualiza la interfaz en el hilo principal
                     getActivity().runOnUiThread(() -> {
-                        Toast.makeText(getActivity(), "Categoría agregada correctamente", Toast.LENGTH_SHORT).show();
-                        // Limpiar el EditText y recargar las categorías
-                        editTextCategoria.setText("");
-                        obtenerCategoriasDesdeServidor();  // Vuelve a obtener las categorías del servidor
+                        Toast.makeText(getActivity(), "Género agregado correctamente", Toast.LENGTH_SHORT).show();
                     });
                 } else {
                     getActivity().runOnUiThread(() -> {
-                        Toast.makeText(getActivity(), "Error al agregar la categoría", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), "Error al agregar el género", Toast.LENGTH_SHORT).show();
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                getActivity().runOnUiThread(() -> {
+                    Toast.makeText(getActivity(), "Error en la conexión", Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+    }
+
+    private void agregarTipoDocumento() {
+        String url = servidor + "itemsController/tipo_documento/agregar_tipo_documento.php";
+
+        String nom_tipo_documento = editTextTipoDocumento.getText().toString();
+
+        RequestParams params = new RequestParams();
+        params.put("nom_tipo_documento", nom_tipo_documento);
+
+        client.post(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String response = new String(responseBody);
+                if (response.contains("success")) {
+                    // Actualiza la interfaz en el hilo principal
+                    getActivity().runOnUiThread(() -> {
+                        Toast.makeText(getActivity(), "Tipo Documento agregado correctamente", Toast.LENGTH_SHORT).show();
+                    });
+                } else {
+                    getActivity().runOnUiThread(() -> {
+                        Toast.makeText(getActivity(), "Error al agregar el tipo documento", Toast.LENGTH_SHORT).show();
                     });
                 }
             }
